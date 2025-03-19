dab04154f997d1afbbaba23e4f1c01dec43dd3b7
nyanlin <nyan101355@gmail.com>
2025-03-19 13:25:08 +0630

    stage change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,10 +3,10 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+    <div class="tw-py-12">
+        <div class="tw-max-w-7xl tw-mx-auto sm:tw-px-6 lg:tw-px-8">
+            <div class="tw-bg-white dark:tw-bg-gray-800 tw-overflow-hidden tw-shadow-sm sm:tw-rounded-lg">
+                <div class="tw-p-6 tw-text-gray-900 dark:tw-text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
